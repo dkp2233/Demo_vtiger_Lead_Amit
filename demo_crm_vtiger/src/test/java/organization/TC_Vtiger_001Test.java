f17de314af54b3fb46e6089203f183e3bb4d9e3f
Admin <Admin@DESKTOP-FFALLFF>
2025-09-24 14:51:55 +0530

    do some changes in organization moduels

--- demo_crm_vtiger/src/test/java/organization/TC_Vtiger_001Test.java
+++ demo_crm_vtiger/src/test/java/organization/TC_Vtiger_001Test.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import com.crm.genericUtility.FileUtility;
@@ -14,26 +15,9 @@ public class TC_Vtiger_001Test {
 	FileUtility flib=new FileUtility();
 	WebDriver driver=null;
 	@Test
-	public void loginToVtigerapplicationTest() {
+	public void demo1Test() {
 		
-	
-		//give implicit time using reusable method because hardcoding is not allow in automation
-		WebDriverUtility wu=new WebDriverUtility();
-		wu.waitForPageToLoad(driver);
-		
-		//create object of login page because we want a element which are present in login page 
-		//user text field,password test field,login button here we are using pom design technique
-		//to store the current address of webElement and avoid StaleElementRefrenceException
-		LoginPage lp=new LoginPage(driver);
-		lp.getusernametextfield().clear();
-		lp.getusernametextfield().sendKeys(username);//give by the user in runtime using cmd line
-		
-		lp.getpasswordtextfield().clear();
-		lp.getpasswordtextfield().sendKeys(password);//give by the user in run time
-		lp.getloginbutton().click();
-		
-		
-		
-	}
+	Reporter.log("demo test 1 run for demo vtigr",true);
+		}
 
 }
--- demo_crm_vtiger/src/test/java/organization/TC_Vtiger_002Test.java
+++ demo_crm_vtiger/src/test/java/organization/TC_Vtiger_002Test.java
@@ -3,6 +3,7 @@ package organization;
 import java.io.IOException;
 
 import org.apache.poi.EncryptedDocumentException;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import com.crm.genericUtility.ExcelUtility;
@@ -12,5 +13,6 @@ public class TC_Vtiger_002Test {
 public void fetchAllDataFromExcelUseingUserDefineMethod() throws EncryptedDocumentException, IOException {
 	ExcelUtility exutil=new ExcelUtility();
 	System.out.println(exutil.getMultipleDataFromExcel("org"));
-}
+	Reporter.log("demo test for demo vtiger",true);
+	}
 }
